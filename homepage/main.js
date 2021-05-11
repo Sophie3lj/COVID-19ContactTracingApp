a6e47427aeef89a72deb330de7149bfeeec7c796
abisparnon <abisparnon@gmail.com>
2021-05-11 05:10:07 +0000

    Updated Menu bar

---
+++ homepage/main.js
@@ -0,0 +1,12 @@
+
+
+var vueinst = new Vue({
+    el: "#home",
+    data: {
+        main_menu: [
+            {title:"Map", url:"/map"},
+            {title:"Check-In", url:"/checkin"},
+            {title:"Check-In History", url:"/checkinhistory"}
+            ]
+    }
+});
