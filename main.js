78f06a46b85a89619ddfc941e88f530d708b7255
Sophie3lj <sophie.3lj@gmail.com>
2021-05-13 12:44:32 +0000

    fixing minimap margins

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var vueinst = new Vue({
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic29waGllM2xqIiwiYSI6ImNrb2R5YXNoazA2MHMybm80cHVsdzRzY3oifQ.BlVnpNJHCi01_yqgo7ZexA';
 var map = new mapboxgl.Map({
-    container: 'map', // container ID
+    container: 'minimap', // container ID
     style: 'mapbox://styles/sophie3lj/ckojqizu24trv18kefcc9vu80', // style URL
     center: [138.599503, -34.921230], // starting position [lng, lat]
     zoom: 10 // starting zoom
