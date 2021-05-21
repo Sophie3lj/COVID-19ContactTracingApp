7d7f9e132f8217fa8484dadd8b5b6278e02925be
Abigail <a1740459@student.adelaide.edu.au>
2021-05-21 02:00:28 +0000

    INstalled mysql package

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mysql = require('mysql');
+
+var dbConnectionPool = mysql.createPool({
+    host: 'localhost',
+    database: 'ct_app'
+});
+
 var app = express();
+
+app.use(function(req, res, next){
+    req.pool = dbConnectionPool;
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
