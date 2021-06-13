2de32dbf17386ee1365d904a76e9fce09e222bf8
abisparnon <abisparnon@gmail.com>
2021-06-13 13:20:56 +0000

    Sing up now logs you in, im a god

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,10 @@ var session;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
     res.render('index', { title: 'Express', });
 });
 
 router.get('/mapCheckins', function(req, res) {
-
     req.pool.getConnection(function(err,connection) {
         if (err) {
             res.sendStatus(500);
@@ -169,55 +167,56 @@ router.post('/deleteHotspot', function(req, res){
 });
 
 router.post('/SignUp', function (req, res, next){
+    var reqBody = req.body;
+    var venue_name = reqBody.venue_name;
+    var first_name = reqBody.first_name;
+	var last_name = reqBody.last_name;
+	var email = reqBody.email;
+	var phone_number = reqBody.phone_number;
+	var street_number = reqBody.street_number;
+	var street_address = reqBody.street_address;
+	var suburb = reqBody.suburb;
+	var post_code = reqBody.post_code;
+	var state = reqBody.state;
+	var password = reqBody.password;
+	var type = "USER";
+	var id = 2;
+	var venueid = 1;
     req.pool.getConnection(function(err,connection){
         if(err){
             console.log(err);
             return;
         }
 
-        var reqBody = req.body;
-        var venue_name = reqBody.venue_name;
-        var first_name = reqBody.first_name;
-		var last_name = reqBody.last_name;
-		var email = reqBody.email;
-		var phone_number = reqBody.phone_number;
-		var street_number = reqBody.street_number;
-		var street_address = reqBody.street_address;
-		var suburb = reqBody.suburb;
-		var post_code = reqBody.post_code;
-		var state = reqBody.state;
-		var password = reqBody.password;
-		var type = "USER";
-		var id = 2;
-
 		var queryString = '';
 
-			bcrypt.genSalt(10, function(err, salt){
+		bcrypt.genSalt(10, function(err, salt){
+        if(err){
+            console.log(err);
+        }
+        bcrypt.hash(password, salt, function(err, hash){
             if(err){
                 console.log(err);
             }
-            bcrypt.hash(password, salt, function(err, hash){
+            password = hash;
+            queryString = "INSERT INTO accounts ( user_type, email, first_name, last_name, password_hash, phone_number) VALUES (?, ?, ?, ?, ?, ?)";
+            connection.query(queryString,[type,email,first_name,last_name,password,phone_number], function(err, result){
                 if(err){
                     console.log(err);
-                }
-                password = hash;
-                queryString = "INSERT INTO accounts ( user_type, email, first_name, last_name, password_hash, phone_number) VALUES (?, ?, ?, ?, ?, ?)";
-                connection.query(queryString,[type,email,first_name,last_name,password,phone_number], function(err, result){
-                    if(err){
-                        console.log(err);
-                    }else {
-                        console.log("New user created");
-                        connection.query("select id FROM accounts ORDER BY ID DESC LIMIT 1", function(err, last_account_result){
-                            if(err){
-                                console.log(err);
-                            }
-                            req.session.email = email;
-                            req.session.user_type = type;
-                            req.session.user_id = last_account_result[0].id;
-                            req.session.user_name = first_name + ' ' + last_name;
-                        });
+                }else {
+                    console.log("New user created");
+                    connection.query("select id FROM accounts ORDER BY ID DESC LIMIT 1", function(err, last_account_result){
+                        if(err){
+                            console.log(err);
+                        }
+                        id = last_account_result[0].id;
+                        req.session.email = email;
+                        req.session.user_type = type;
+                        req.session.user_id = last_account_result[0].id;
+                        req.session.user_name = first_name + ' ' + last_name;
+                    });
 
-                    }
+                }
 
 
 		if(suburb === ""){
@@ -294,12 +293,30 @@ router.post('/SignUp', function (req, res, next){
                 });
 			});
 
-		});
+        connection.query("SELECT id FROM venues ORDER BY ID DESC LIMIT 1", function(err, last_account_result){
+            if(err){
+                console.log(err);
+            }else{
+                venueid = last_account_result[0].id;
+            }
+        });
+	});
 
+    req.session.email = email;
+    req.session.user_type = type;
+
+    if(type !== 'VENUE'){
+        req.session.user_id = id;
+        req.session.user_name = first_name + ' ' + last_name;
+    }
+    else{
+        req.session.user_id = id;
+        req.session.venue_id = venue_id;
+        req.session.user_name = venue_name;
+    }
 
     res.redirect("/");
 });
-
 
 
 router.post('/newadmin', function (req, res, next){
